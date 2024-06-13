6b40eb77b18088b77200799905f10a450bb25955
Zaman Ehsani <zamanehsani@gmail.com>
2024-06-13 19:44:18 +0400

    installed heroicon

--- frontend/src/components/graph/index.tsx
+++ frontend/src/components/graph/index.tsx
@@ -4,12 +4,11 @@ import Stats from "./stats";
 
 function GraphWindow() {
   
-  return ( 
-    <div className="bg-white p-6 rounded-lg shadow-md">
-    <h2 className="text-xl font-semibold mb-4">Page Graph</h2>
-    <p>This is the graph content</p>
-  </div>
-  );
+  return (
+    <div className="flex flex-wrap">
+      <Stats />
+    </div>
+   );
 }
 
 export default GraphWindow;
--- frontend/src/components/graph/stats.tsx
+++ frontend/src/components/graph/stats.tsx
@@ -1,9 +1,22 @@
 
 
 function Stats() {
-  return ( <div className="flex">
-    <p>the three top stats comes here</p>
-  </div> );
+  return ( 
+    <div className="flex flex-nowrap justify-center gap-4">
+      <div className="w-1/3 bg-gray-200 p-4 rounded-lg shadow-md">
+        <h2 className="text-lg font-semibold mb-2">Column 1</h2>
+        <p>Content for column 1</p>
+      </div>
+      <div className="w-1/3 bg-gray-200 p-4 rounded-lg shadow-md">
+        <h2 className="text-lg font-semibold mb-2">Column 2</h2>
+        <p>Content for column 2 </p>
+      </div>
+      <div className="w-1/3 bg-gray-200 p-4 rounded-lg shadow-md">
+        <h2 className="text-lg font-semibold mb-2">Column 3</h2>
+        <p>Content for column 3 </p>
+      </div>
+    </div>
+  );
 }
 
 export default Stats;
